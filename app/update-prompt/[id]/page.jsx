f6261e6beff6701045e871fbce40ec078d13e6f1
raphael <you@example.com>
2024-02-21 10:32:59 +0100

    Fingers crossed

--- app/update-prompt/[id]/page.jsx
+++ app/update-prompt/[id]/page.jsx
@@ -1,34 +1,34 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter,  } from "next/navigation";
 import Form from "@components/Form";
-const EditPrompt = () => {
+const EditPrompt = ({params}) => {
   const router = useRouter();
   const [submitting, setSubmitting] = useState(false);
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
+  // const searchParams = useSearchParams();
+  // const params.id = searchParams.get("id");
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
   useEffect(() => {
     const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
+      const response = await fetch(`/api/prompt/${params.id}`);
       const data = await response.json();
       setPost({
         prompt: data.prompt,
         tag: data.tag,
       });
     };
-    if (promptId) getPromptDetails();
-  }, [promptId]);
+    if (params.id) getPromptDetails();
+  }, [params.id]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
-    if (!promptId) return alert("Prompt Id not found");
+    if (!params.id) return alert("Prompt Id not found");
     try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
+      const response = await fetch(`/api/prompt/${params.id}`, {
         method: "PATCH",
         body: JSON.stringify({
           prompt: post.prompt,
