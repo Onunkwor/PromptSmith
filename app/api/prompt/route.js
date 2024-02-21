8cdb7bac9a86625b01ec22732337dc7a35c0c7ee
raphael <you@example.com>
2024-02-21 12:45:17 +0100

    fixed bug

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,17 @@ export const GET = async (req) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+
+    // Set cache-control headers to prevent caching
+    const headers = {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+      "Pragma": "no-cache",
+      "Expires": "0"
+    };
+
+    // Return the response with cache-control headers
+    return new Response(JSON.stringify(prompts), { status: 200, headers });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
