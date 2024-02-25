b1f8b707158b4f03ef120ec270eaf1f087d4ca49
raphael <you@example.com>
2024-02-25 12:36:08 +0100

    Switched to server actions when creating prompts

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Form from "@components/Form";
+import { createNewPrompt } from "@lib/actions/prompt.actions";
 const CreatePrompt = () => {
   const router = useRouter()
   const {data: session} = useSession()
@@ -14,17 +15,15 @@ const CreatePrompt = () => {
   const createPrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
-
+     const promptData = {
+      prompt: post.prompt,
+      userId: session.user.id,
+      tag: post.tag,
+     }
+     console.log(promptData);
     try {
-      const response = await fetch("/api/prompt/new", {
-        method: "POST",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          userId: session?.user.id,
-          tag: post.tag,
-        }),
-      });
-      if (response.ok) {
+      const newPrompt = createNewPrompt(promptData)
+      if (newPrompt) {
         router.push("/");
       }
     } catch (error) {
